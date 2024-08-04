0a916df4418dfd460f734c374a8643cf91ccd581
IBTISAM ALI <aliibtisam39@gmail.com>
2024-08-04 06:02:42 +0500

    Portfolio

--- Files/src/components/Card.js
+++ Files/src/components/Card.js
@@ -1,23 +1,33 @@
-import { Box, HStack, Heading, Image, Text, VStack } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
-
-const Card = ({ title, description, imageSrc }) => {
-  return (
-    <>
-      <Box textAlign="left" bg="white" borderRadius="md">
-        <Image src={imageSrc} borderRadius="md"/>
-        <div style={{padding: 25}}>
-        <Heading as="h4" size="md" color="black">
-          {title}
-        </Heading>
-        <Text color="black" py='2'>{description}</Text>
-        <Text color='black'fontSize='sm' >See more <FontAwesomeIcon icon={faArrowRight} size="1x" /></Text>
-        </div>
-      </Box>
-    </>
-  );
-};
-
-export default Card;
+import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"; 
+import React from "react"; 
+ 
+const Card = ({ title, description, imageSrc }) => { 
+   return ( 
+     <VStack 
+       color="black" 
+       backgroundColor="white" 
+       cursor="pointer" 
+       borderRadius="xl" 
+     > 
+       <Image borderRadius="xl" src={imageSrc} alt={title} /> 
+       <VStack spacing={4} p={4} alignItems="flex-start"> 
+         <HStack justifyContent="space-between" alignItems="center"> 
+           <Heading as="h3" size="md"> 
+             {title} 
+           </Heading> 
+         </HStack> 
+         <Text color="#64748b" fontSize="lg"> 
+           {description} 
+         </Text> 
+         <HStack spacing={2} alignItems="center"> 
+           <p>See more</p> 
+           <FontAwesomeIcon icon={faArrowRight} size="1x" /> 
+         </HStack> 
+       </VStack> 
+     </VStack> 
+   ); 
+}; 
+ 
+export default Card; 
